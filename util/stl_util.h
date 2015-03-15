4058fa4e87fbaf64c5787f2db990bd4207eae12b
Derek Snyder <snyderek@users.noreply.github.com>
2015-03-14 23:32:17 -0700

    Add a function that returns a set (or map) with a single element in it. This is sometimes useful in constructors.

--- util/stl_util.h
+++ util/stl_util.h
@@ -64,6 +64,13 @@ typename Container::iterator FindInContainer(
   return it;
 }
 
+template<class Set>
+Set MakeSingletonSet(const typename Set::value_type& element) {
+  Set s;
+  s.insert(element);
+  return s;
+}
+
 }  // namespace floating_temple
 
 #endif  // UTIL_STL_UTIL_H_

a0466fba3043d176d6e0fe7ab32723c79d0103b7
Derek Snyder <derek.stephen.snyder@gmail.com>
2015-03-28 22:07:10 -0700

    Add a local object type for unserializable Python objects.

---
+++ python/unserializable_local_object.cc
@@ -0,0 +1,50 @@
+// Floating Temple
+// Copyright 2015 Derek S. Snyder
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "python/unserializable_local_object.h"
+
+#include "third_party/Python-3.4.2/Include/Python.h"
+
+#include <string>
+
+#include "base/logging.h"
+#include "python/local_object_impl.h"
+#include "python/proto/serialization.pb.h"
+
+using std::string;
+
+namespace floating_temple {
+namespace python {
+
+UnserializableLocalObject::UnserializableLocalObject(PyObject* py_object)
+    : LocalObjectImpl(CHECK_NOTNULL(py_object)) {
+}
+
+LocalObject* UnserializableLocalObject::Clone() const {
+  return new UnserializableLocalObject(py_object());
+}
+
+string UnserializableLocalObject::Dump() const {
+  return "{ \"type\": \"UnserializableLocalObject\" }";
+}
+
+void UnserializableLocalObject::PopulateObjectProto(
+    ObjectProto* object_proto, SerializationContext* context) const {
+  CHECK(object_proto != nullptr);
+  object_proto->mutable_unserializable_object();
+}
+
+}  // namespace python
+}  // namespace floating_temple
---
+++ python/unserializable_local_object.h
@@ -0,0 +1,43 @@
+// Floating Temple
+// Copyright 2015 Derek S. Snyder
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef PYTHON_UNSERIALIZABLE_LOCAL_OBJECT_H_
+#define PYTHON_UNSERIALIZABLE_LOCAL_OBJECT_H_
+
+#include "base/macros.h"
+#include "python/local_object_impl.h"
+
+namespace floating_temple {
+namespace python {
+
+class UnserializableLocalObject : public LocalObjectImpl {
+ public:
+  explicit UnserializableLocalObject(PyObject* py_object);
+
+  LocalObject* Clone() const override;
+  std::string Dump() const override;
+
+ protected:
+  void PopulateObjectProto(ObjectProto* object_proto,
+                           SerializationContext* context) const override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(UnserializableLocalObject);
+};
+
+}  // namespace python
+}  // namespace floating_temple
+
+#endif  // PYTHON_UNSERIALIZABLE_LOCAL_OBJECT_H_

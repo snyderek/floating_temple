110d1b491194adf60e97d89584186c085a6c9e20
Derek Snyder <snyderek@users.noreply.github.com>
2015-04-02 22:38:33 -0700

    Alphabetize the hook functions in floating_temple_hooks.{h,c}.

--- third_party/Python-3.4.2/Include/floating_temple_hooks.h
+++ third_party/Python-3.4.2/Include/floating_temple_hooks.h
@@ -7,14 +7,14 @@ extern "C" {
 
 typedef PyObject *(*object_creation_hook_func)(PyObject *);
 
-extern object_creation_hook_func long_creation_hook;
 extern object_creation_hook_func list_creation_hook;
-
-PyAPI_FUNC(object_creation_hook_func)
-Py_InstallLongCreationHook(object_creation_hook_func new_hook);
+extern object_creation_hook_func long_creation_hook;
 
 PyAPI_FUNC(object_creation_hook_func)
 Py_InstallListCreationHook(object_creation_hook_func new_hook);
+
+PyAPI_FUNC(object_creation_hook_func)
+Py_InstallLongCreationHook(object_creation_hook_func new_hook);
 
 
 #ifdef __cplusplus

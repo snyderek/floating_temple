060812dc5b6fe3c2f0005da3559af6694153fae5
Derek Snyder <snyderek@users.noreply.github.com>
2016-05-28 19:12:51 -0700

    Modify the DISALLOW_COPY_AND_ASSIGN macro to use C++11's '= delete' syntax.

--- base/macros.h
+++ base/macros.h
@@ -4,29 +4,10 @@
 #ifndef BASE_MACROS_H_
 #define BASE_MACROS_H_
 
-// From Google C++ Style Guide
-// http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml
-// Accessed February 8, 2013
-//
-// A macro to disallow the copy constructor and operator= functions
-// This should be used in the private: declarations for a class
-//
-// TODO(dss): Use C++11's "= delete" syntax to suppress the creation of default
-// class members.
+// A macro to disallow the copy constructor and operator= functions.
 #define DISALLOW_COPY_AND_ASSIGN(TypeName) \
-  TypeName(const TypeName&);               \
-  void operator=(const TypeName&)
-
-// Example from the Google C++ Style Guide:
-//
-// class Foo {
-//  public:
-//   Foo(int f);
-//   ~Foo();
-//
-//  private:
-//   DISALLOW_COPY_AND_ASSIGN(Foo);
-// };
+  TypeName(const TypeName&) = delete; \
+  void operator=(const TypeName&) = delete
 
 // A macro to compute the number of elements in a statically allocated array.
 #define ARRAYSIZE(a) (sizeof(a) / sizeof((a)[0]))
